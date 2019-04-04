d94f8ba9490b0aa2f9f4282589ab88db7594e1f8
HeeSeungPC <moss2223@kpu.ac.kr>
2019-04-04 23:58:57 +0900

    19.04.04 3DGP // FROM PC

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,10 +1,18 @@
-import math
+import copy
 
-def a():
-	x1, y1, x2, y2, x3, y3 = eval(input("삼각형의 세 꼭짓점을 입력하세요: "))
-	a = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
-	b = math.sqrt(math.pow((x3 - x2), 2) + math.pow((y3 - y2), 2))
-	c = math.sqrt(math.pow((x1 - x3), 2) + math.pow((y1 - y3), 2))
-	s = (a+b+c) /2
-	area = math.sqrt(s * (s - a) * (s - b) * (s - c))
-	print("삼각형의 넓이는" + area + "입니다.")
+def isSorted(lst):
+    temp = copy.deepcopy(lst)
+    temp.sort()
+    for i in range(0, len(lst)):
+        if temp[i] != lst[i]:
+            print("리스트는 정렬되어 있지 않습니다.")
+            break
+        elif (i+1) == len(lst):
+            print("리스트는 정렬되어 있습니다.")
+
+
+s = input("리스트를 입력하세요:")
+lst = s.split()
+lst = [eval(i) for i in lst]
+
+isSorted(lst)
